2d39bbc6f96c191758ffcc51ec975430b6ff20ea
Thomas Findlay <thomasfindlay94@gmail.com>
2022-11-26 22:21:47 +0000

    Fixed file paths in the postcss config. Files for tailwind and styleling had incorrect extensions

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   plugins: [
     require('postcss-flexbugs-fixes'),
     require('stylelint')({
-      configFile: 'stylelint.config.js',
+      configFile: 'stylelint.config.cjs',
     }),
     require('postcss-import'),
     require('postcss-extend'),
@@ -12,7 +12,7 @@ module.exports = {
     // Comment out postcss-nested if you're using tailwindcss/nesting
     require('postcss-nested'),
     require('tailwindcss/nesting'),
-    require('tailwindcss')('tailwind.config.js'),
+    require('tailwindcss')('tailwind.config.cjs'),
     require('postcss-preset-env', {
       autoprefixer: {
         flexbox: 'no-2009',
