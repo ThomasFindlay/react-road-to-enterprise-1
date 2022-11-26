639416fd94d39a5ce908483c9ef119b9921a0cc8
Casey <leptone55@gmail.com>
2022-11-26 11:46:26 -0600

    inital

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  // Add only if you want to use the @tailwindcss/forms package
+  plugins: [require('@tailwindcss/forms')],
+}
